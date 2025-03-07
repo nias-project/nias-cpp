c369325d73b0c0634ee6b182f8e23df3bf44a974
Tobias Leibner <7058290+tobiasleibner@users.noreply.github.com>
2025-03-07 22:47:59 +0100

    Add job to test inclusion via FetchContent

--- cmake/nias_cpp-config.cmake
+++ cmake/nias_cpp-config.cmake
@@ -43,14 +43,18 @@ if(NOT COMMAND pybind11_add_module)
         COMMAND ${UV_EXECUTABLE} run --no-project --with toml cmake/parse_pyproject_toml.py pybind11
         WORKING_DIRECTORY ${_NIAS_CPP_DIR}
         OUTPUT_VARIABLE PYBIND11_VERSION
-        ERROR_VARIABLE _pybind11_version_error)
+        RESULT_VARIABLE _pybind11_version_result)
 
     string(FIND "${PYBIND11_VERSION}" "Error:" _pybind11_error_in_result)
-    if(_pybind11_version_error OR _pybind11_error_in_result GREATER -1)
+    if(_pybind11_version_result OR _pybind11_error_in_result GREATER -1)
         set(_error_line1 "Could not parse pyproject.toml to get pybind11 version.\n")
         # only one of these variables should not be empty
-        set(_error_line2 "The following error occurred: ${_pybind11_version_error}${PYBIND11_VERSION}")
-        message(WARNING "${_error_line1}${_error_line2}")
+        message(WARNING "Error message1: ${_pybind11_version_result}")
+        message(WARNING "Error message2: ${PYBIND11_VERSION}")
+        # set(_error_line2 "The following error occurred: ${_pybind11_version_error}${PYBIND11_VERSION}")
+        # message(WARNING "${_error_line1}${_error_line2}")
+        set(PYBIND11_VERSION 2.13.6)
+        message(WARNING "Setting pybind11 version to default value ${PYBIND11_VERSION}.")
     endif()
 
     # add pybind11
