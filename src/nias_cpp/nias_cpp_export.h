0cc8250ac02c3c8ec4742bf27b3393f653e8fca4
Tobias Leibner <7058290+tobiasleibner@users.noreply.github.com>
2025-12-01 12:26:02 +0100

    Fix undefined symbol errors on Windows

--- src/nias_cpp/nias_cpp_export.h
+++ src/nias_cpp/nias_cpp_export.h
@@ -2,14 +2,18 @@
 #define NIAS_CPP_EXPORT_H
 
 #ifndef NIAS_CPP_EXPORT
-#    ifdef _WIN32
-#        ifdef nias_cpp_EXPORTS
-#            define NIAS_CPP_EXPORT __declspec(dllexport)
-#        else
-#            define NIAS_CPP_EXPORT __declspec(dllimport)
-#        endif
+#    ifdef NIAS_CPP_STATIC
+#        define NIAS_CPP_EXPORT
 #    else
-#        define NIAS_CPP_EXPORT __attribute__((visibility("default")))
+#        ifdef _WIN32
+#            ifdef nias_cpp_EXPORTS
+#                define NIAS_CPP_EXPORT __declspec(dllexport)
+#            else
+#                define NIAS_CPP_EXPORT __declspec(dllimport)
+#            endif
+#        else
+#            define NIAS_CPP_EXPORT __attribute__((visibility("default")))
+#        endif
 #    endif
 #endif
 
