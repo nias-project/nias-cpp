036c0146ecfc20cc5964b51d7c4b48702749f18e
Tobias Leibner <7058290+tobiasleibner@users.noreply.github.com>
2025-12-01 15:59:18 +0100

    Reenable hidden visibility preset

--- src/nias_cpp/nias_cpp_export.h
+++ src/nias_cpp/nias_cpp_export.h
@@ -2,20 +2,21 @@
 #define NIAS_CPP_EXPORT_H
 
 #ifndef NIAS_CPP_EXPORT
-#    ifdef NIAS_CPP_STATIC
-#        define NIAS_CPP_EXPORT __attribute__((visibility("default")))
-#        define NIAS_CPP_DLL_LOCAL __attribute__((visibility("hidden")))
-#    else
-#        ifdef _WIN32
+#    ifdef _WIN32
+#        ifdef NIAS_CPP_STATIC
+#            define NIAS_CPP_EXPORT
+#            define NIAS_CPP_DLL_LOCAL
+#        else
 #            ifdef nias_cpp_EXPORTS
 #                define NIAS_CPP_EXPORT __declspec(dllexport)
 #            else
 #                define NIAS_CPP_EXPORT __declspec(dllimport)
 #            endif
-#        else
-#            define NIAS_CPP_EXPORT __attribute__((visibility("default")))
-#            define NIAS_CPP_DLL_LOCAL __attribute__((visibility("hidden")))
+#            define NIAS_CPP_DLL_LOCAL
 #        endif
+#    else  // Linux
+#        define NIAS_CPP_EXPORT __attribute__((visibility("default")))
+#        define NIAS_CPP_DLL_LOCAL __attribute__((visibility("hidden")))
 #    endif
 #endif
 
